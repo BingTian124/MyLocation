3c073d2cfbf6a0dd1c7b0a409283a473355a14f5
BingTian <chittytian93@gmail.com>
2019-02-09 17:34:30 -0800

    Initial Commit

---
+++ MyLocation/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocation
+//
+//  Created by Bing Tian on 2/9/19.
+//  Copyright © 2019 tianbing. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MyLocation/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocation
+//
+//  Created by Bing Tian on 2/9/19.
+//  Copyright © 2019 tianbing. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
